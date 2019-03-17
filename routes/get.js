83a379e4a12284e5ebdc1beaeab9eca38a01f91b
Dominic Evans <dominic@bitt.com>
2019-03-16 22:16:49 -0400

    add fitbit callback

--- routes/get.js
+++ routes/get.js
@@ -1,118 +1,143 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const router = express.Router()
+const router = express.Router();
 // const jwt = require('jsonwebtoken')
 // const axios = require('axios');
 // var bodyParser = require('body-parser')
-const path = require('path')
-const withingsAuth = require('../withings/auth')
-const withingsData = require('../withings/fetchdata')
-const formatMLData = require('../machineLearning/formatData')
-const basepyUrl = path.join(__dirname, '../machineLearning/base.py');
-
+const path = require("path");
+const withingsAuth = require("../withings/auth");
+const withingsData = require("../withings/fetchdata");
+const fitbitAuth = require("../fitbit/auth");
+const fitbitData = require("../fitbit/fetchdata");
+const formatMLData = require("../machineLearning/formatData");
+const basepyUrl = path.join(__dirname, "../machineLearning/base.py");
 
 // =============================== Withings ================================>
-router.get('/api/withings', (req, res)=>{
-    res.json({
-        message: 'welcome'
+router.get("/api/withings", (req, res) => {
+  res.json({
+    message: "welcome"
+  });
+});
+
+router.get("/api/withings/auth", (req, res) => {
+  let AccessCode = req.query.code;
+  let uid = req.query.state;
+  if (AccessCode) {
+    withingsAuth
+      .AccessToken(AccessCode, uid)
+
+      .then(resp => {
+        console.log(resp);
+        if (resp.fbstatus === 200)
+          res.sendFile("success.html", {
+            root: path.join(__dirname, "../public/withingsAuth")
+          });
+        else
+          res.sendFile("failure.html", {
+            root: path.join(__dirname, "../public/withingsAuth")
+          });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  } else {
+    res.sendFile("failure.html", {
+      root: path.join(__dirname, "../public/withingsAuth")
+    });
+  }
+});
+
+router.get("/api/withings/refresh_token", (req, res) => {
+  let refToken = req.query.RefreshToken;
+  let uid = req.query.Uid;
+  withingsAuth
+    .RefreshToken(refToken, uid)
+
+    .then(resp => {
+      res.json({
+        resp
+      });
     })
-})
-
-router.get('/api/withings/auth', (req, res) => {
-    let AcessCode = req.query.code
-    let uid = req.query.state;
-    // console.log(AcessCode)
-    if (AcessCode) {
-        withingsAuth.AccessToken(AcessCode, uid)
-        
-        .then((resp) => {
-            console.log(resp)
-            if (resp.fbstatus === 200)
-                res.sendFile('success.html', { root: path.join(__dirname, '../public/withingsAuth') })
-            else
-                res.sendFile('failure.html', { root: path.join(__dirname, '../public/withingsAuth') })
-        })
-        .catch(err=>{console.log(err)})
-    }
-    else {
-        res.sendFile('failure.html', { root: path.join(__dirname, '../public/withingsAuth') })
-    }
+    .catch(err => {
+      console.log(err);
+    });
 });
 
-router.get('/api/withings/refresh_token', (req, res) => {
-    let refToken = req.query.RefreshToken
-    let uid = req.query.Uid;
-    withingsAuth.RefreshToken(refToken, uid)
+router.get("/api/withings/fetchdata", (req, res) => {
+  let accesstoken = req.query.access_token;
+  let uid = req.query.Uid;
+  let date = req.query.date;
 
-        .then((resp) => {
-            res.json({
-                resp
-            })
-        })
-        .catch(err => { console.log(err) })
-});
-
-
-router.get('/api/withings/fetchdata', (req, res) => {
-    let accesstoken = req.query.access_token
-    let uid = req.query.Uid;
-    let date = req.query.date;
-
-    withingsData.getBPData(accesstoken, uid, date)
-        .then((resp) => {
-            res.json({
-                response: resp
-            })
-        })
-        .catch(err => { console.log(err) })
+  withingsData
+    .getBPData(accesstoken, uid, date)
+    .then(resp => {
+      res.json({
+        response: resp
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 // ============================ Fitbit =======================
 
-router.get('/api/fitbit/fetchdata', (req, res) => {
-    let accesstoken = req.query.access_token
-    let uid = req.query.Uid;
-    let date = req.query.date;
+router.get("/api/fitbit/auth", (req, res) => {
+  let AccessCode = req.query.code;
+  let uid = req.query.state;
+  if (AccessCode) {
+    fitbitAuth
+      .AccessToken(AccessCode, uid)
 
-    withingsData.getBPData(accesstoken, uid, date)
-        .then((resp) => {
-            res.json({
-                response: resp
-            })
-        })
-        .catch(err => { console.log(err) })
+      .then(resp => {
+        console.log(resp);
+        if (resp.fbstatus === 200)
+          res.sendFile("success.html", {
+            root: path.join(__dirname, "../public/fitbitAuth")
+          });
+        else
+          res.sendFile("failure.html", {
+            root: path.join(__dirname, "../public/fitbitAuth")
+          });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  } else {
+    res.sendFile("failure.html", {
+      root: path.join(__dirname, "../public/fitbitAuth")
+    });
+  }
 });
 
 // ============================ ML ============================
 
-router.get('/api/ml', (req, res)=>{
+router.get("/api/ml", (req, res) => {
+  let runPy = new Promise((success, nosuccess) => {
+    const { spawn } = require("child_process");
+    const pyprog = spawn("python", [basepyUrl]);
 
-    let runPy = new Promise( (success, nosuccess) => {
-        
-        const { spawn } = require('child_process');
-        const pyprog = spawn('python', [basepyUrl]);
-
-        pyprog.stdout.on('data', (data) => {
-            success(data);
-        });
-        pyprog.stderr.on('data', (data) => {
-            nosuccess(data);
-        });
+    pyprog.stdout.on("data", data => {
+      success(data);
     });
-    
-    runPy.then((resp) =>{
-            let response = formatMLData.formatData(resp);
-            res.json({
-                response,
-            });
-        })
-        .catch((err)=>{
-            let error = formatMLData.formatData(err)
-            res.json({
-                error,
-            });
-        })
+    pyprog.stderr.on("data", data => {
+      nosuccess(data);
+    });
+  });
+
+  runPy
+    .then(resp => {
+      let response = formatMLData.formatData(resp);
+      res.json({
+        response
+      });
+    })
+    .catch(err => {
+      let error = formatMLData.formatData(err);
+      res.json({
+        error
+      });
+    });
 });
 
-
-module.exports = router
+module.exports = router;
--- withings/auth.js
+++ withings/auth.js
@@ -1,8 +1,6 @@
 const firebase = require('firebase-admin')
-var serviceAccount = require('../firebase/adminsdk.json');
-var rp = require('request-promise');
-
-
+const serviceAccount = require('../firebase/adminsdk.json');
+const rp = require('request-promise');
 
 firebase.initializeApp({
     credential: firebase.credential.cert(serviceAccount),
@@ -10,15 +8,13 @@ firebase.initializeApp({
 });
 
 const db = firebase.database();
-
 const client_id = '5d81605593c6c4e8e1c3871f69fa3ed026659338266b7e27ba07a352bfb6d7fb'
 const client_secret = '2798f13818cc213ccce23a4c7c6e0e107a2156ff9aeffb275bc8e9eccde4dd63'
 const redirect_uri = "https://atria.coach/api/withings/auth"
 const TokenURL = "https://account.withings.com/oauth2/token"
-let AccessObj;
 
 
-var AccessToken = (withingscode, firebaseUID) =>{
+const AccessToken = (withingscode, firebaseUID) =>{
     const requestBody = {
         client_id,
         client_secret,
@@ -44,7 +40,7 @@ var AccessToken = (withingscode, firebaseUID) =>{
         });
 }
 
-var RefreshToken = (refresh_token, firebaseUID) => {
+const RefreshToken = (refresh_token, firebaseUID) => {
     const requestBody = {
         client_id,
         client_secret,
@@ -69,7 +65,7 @@ var RefreshToken = (refresh_token, firebaseUID) => {
 }
 
 
-var WriteToDb = (firebaseUID, AuthObj) =>{
+const WriteToDb = (firebaseUID, AuthObj) =>{
     return new Promise ((resolve, reject) => { 
         let user = db.ref('users/'+firebaseUID)        
         user.update({
