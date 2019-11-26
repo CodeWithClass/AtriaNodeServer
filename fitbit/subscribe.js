a8778582b2b28acc8d8f41f1053b8fd700b63897
Dominic Evans <dominic@bitt.com>
2019-11-26 15:25:03 -0400

    test

--- fitbit/subscribe.js
+++ fitbit/subscribe.js
@@ -11,12 +11,9 @@ const AddSubscriber = (subscriptionId, accessToken) => {
     uri: "https://api.fitbit.com/1/user/-/apiSubscriptions/"+subscriptionId+".json",
   }
 
-   rp(requestData)
-    .then(res => { console.log(res)})
-    .catch(err => {
-      console.log(err)
-      return err
-    })
+   return rp(requestData)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
 }
 
 
