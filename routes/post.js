0cdfc2cefccb63dbb7433a8390a537bda242bad1
Dominic Evans <dominic@bitt.com>
2019-11-28 23:16:01 -0400

    looking good

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const fitbitData = require("../fitbit/fetchdata")
+const {ReadFromDb} = require("../helpers/db-helpers")
 
 
 
@@ -15,8 +15,12 @@ router.post('/api/withings/auth', (req, res) => {
 // =============================== fitbit ================================>
 
 router.post("/api/fitbit/webhook", (req, res) => {
-    console.log(req.body)
     res.status(204).send()
+
+    console.log(req.body[0].subscriptionId)
+    const { subscriptionId, collectionType, date}
+    const refresh_token = ReadFromDb(subscriptionId, '/fitbitAuth/refresh_token')
+    req.body
 })
 
 module.exports = router
