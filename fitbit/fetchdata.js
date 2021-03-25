fcaad13f3cb8f640b199d30beba67a1e7c209810
Dominic Evans <dominiccevans@gmail.com>
2021-03-25 11:31:43 -0400

    mychanges

--- fitbit/fetchdata.js
+++ fitbit/fetchdata.js
@@ -6,50 +6,48 @@ const auth = require("./auth")
 const db = firebase.database()
 
 const makeCall = (fitbitUID, accessToken, firebaseUID, date = null) => {
-  if (!date)
-    date =
-      fullDate.getFullYear() +
-      "-" +
-      (fullDate.getMonth() + 1) +
-      "-" +
-      fullDate.getDate()
+	if (!date)
+		date =
+			fullDate.getFullYear() +
+			"-" +
+			(fullDate.getMonth() + 1) +
+			"-" +
+			fullDate.getDate()
 
-  const dataURL =
-    "https://api.fitbit.com/1/user/" +
-    fitbitUID +
-    "/activities/date/" +
-    date +
-    ".json"
+	const dataURL =
+		"https://api.fitbit.com/1/user/" +
+		fitbitUID +
+		"/activities/date/" +
+		date +
+		".json"
 
-  const requestData = {
-    method: "GET",
-    headers: {
-      Authorization: "Bearer " + accessToken
-    },
-    uri: dataURL,
-    json: true, // Automatically stringifies the body to JSON
-    resolveWithFullResponse: true
-  }
+	const requestData = {
+		method: "GET",
+		headers: {
+			Authorization: "Bearer " + accessToken,
+		},
+		uri: dataURL,
+		json: true, // Automatically stringifies the body to JSON
+		resolveWithFullResponse: true,
+	}
 
-  return rp(requestData)
-    .then(res => {
-      if (res.statusCode === 200)
-        return WriteToDb(firebaseUID, date, res.body)
-      else
-        return res
-    })
-    .catch(err => {
-      return err.response
-    })
+	return rp(requestData)
+		.then((res) => {
+			if (res.statusCode === 200) return WriteToDb(firebaseUID, date, res.body)
+			else return res
+		})
+		.catch((err) => {
+			return err.response
+		})
 }
 
 const WriteToDb = (firebaseUID, date, fitbitData = {}) => {
-  return new Promise((resolve, reject) => {
-    let user = db.ref("users/" + firebaseUID + "/dailyStats/" + date.toString())
-    user.update({
-      fitbit: fitbitData
-    })
-    resolve(fitbitData)
-  })
+	return new Promise((resolve, reject) => {
+		let user = db.ref("users/" + firebaseUID + "/dailyStats/" + date.toString())
+		user.update({
+			fitbit: fitbitData,
+		})
+		resolve(fitbitData)
+	})
 }
 module.exports = { makeCall }
--- routes/post.js
+++ routes/post.js
@@ -1,45 +1,28 @@
-const express = require('express');
-const router = express.Router();
-const jwt = require('jsonwebtoken');
-const checkauth = require('./checkauth')
+const express = require("express")
+const router = express.Router()
+const jwt = require("jsonwebtoken")
 
-router.post('/api/login', (req, res) => {
-    //mocker user
-    const user = {
-        id: 1,
-        username: 'brad',
-        email: 'brad@gmail.com'
-    }
+router.post("/api/login", (req, res) => {
+	//mocker user
+	const user = {
+		id: 1,
+		username: "brad",
+		email: "brad@gmail.com",
+	}
 
-    jwt.sign({ user }, 'secretkey', (err, token) => {
-        res.json({
-            token,
-            hi: 'jsdsd'
-        })
-    });
-
-})
-// console.log(checkauth.verifyToken)
-
-router.post('/api/post', (req, res) => {
-
-    const routeAuth = () => checkauth.verifyToken(req, 'secretkey', res);
-
-    if(routeAuth()){
-        ///do stuff
-    }else{
-        //unable to do stuff
-    }
-    
+	jwt.sign({ user }, "secretkey", (err, token) => {
+		res.json({
+			token,
+			hi: "jsdsd",
+		})
+	})
 })
 
 // =============================== Withings ================================>
-router.post('/api/withings/auth', (req, res) => {
-    res.json({
-        message: 'welcome to post'
-    })
+router.post("/api/withings/auth", (req, res) => {
+	res.json({
+		message: "welcome to post",
+	})
 })
-
-
 
 module.exports = router
